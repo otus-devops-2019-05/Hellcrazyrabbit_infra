ed75889aecfce529814ffc086b9b867a1a0cc1b4
Dmitrii Sedov <vendikato@gmail.com>
2019-06-27 21:21:58 +0300

    Making all HW05 done

---
+++ config-script/create-reddit-vm.sh
@@ -0,0 +1,8 @@
+gcloud compute instances create reddit-app-new \
+--zone=us-west1-b \
+--boot-disk-size=10GB \
+--boot-disk-type=pd-ssd \
+--machine-type=f1-micro \
+--tags puma-server \
+--image=reddit-full-1561658948 \
+--restart-on-failure
