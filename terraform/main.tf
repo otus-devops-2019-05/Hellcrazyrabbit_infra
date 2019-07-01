af98662f91f77ec5571c872e2307719f2f8e9259
Dmitrii Sedov <vendikato@gmail.com>
2019-07-01 15:03:33 +0300

    HW06 finishing. Configuring variables, example for git. Terraform files fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,63 +1,76 @@
 terraform {
-# Версия terraform
-required_version = "0.11.11"
+  # Версия terraform
+  required_version = "0.11.11"
 }
 
 provider "google" {
-# Версия провайдера
-version = "2.0.0"
-# ID проекта
-project = "prismatic-canto-229410"
-region = "europe-west-1"
+  # Версия провайдера
+  version = "2.0.0"
+
+  # ID проекта
+  project = "${var.project}"
+  region  = "${var.region}"
 }
 
 resource "google_compute_instance" "app" {
-	name = "reddit-app"
-	machine_type = "g1-small"
-	zone = "europe-west1-b"
-	tags = ["reddit-app"]
-	connection {
-		type = "ssh"
-		user = "appuser"
-		agent = false
-		private_key = "${file("~/.ssh/appuser")}"
-	}
-	provisioner "file" {
-		source = "files/puma.service"
-		destination = "tmp/puma.service"
-	}
-	provisioner "remote-exec"{
-		script = "files/deploy.sh"
-	}
-	metadata {
-		#пусть публичного ключа
-		ssh-keys = "appuser:${file("~/.ssh/appuser.pub")}"
-	}
-	# определение загрузочного диска
-	boot_disk {
-		initialize_params {
-		image = "reddit-base-1561656556"
-		}
-	}
-# определение сетевого интерфейса
-	network_interface {
-		# сеть, к которой присоединить данный интерфейс
-		network = "default"
-		# использовать ephemeral IP для доступа из Интернет
-		access_config {}
-	}
+  name         = "reddit-app"
+  machine_type = "g1-small"
+  zone         = "europe-west1-b"
+  tags         = ["reddit-app"]
+
+  connection {
+    type        = "ssh"
+    user        = "appuser"
+    agent       = false
+    private_key = "${file(var.private_key_path)}"
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+
+  metadata {
+    #пусть публичного ключа
+    ssh-keys = "appuser:${file(var.public_key_path)}"
+  }
+
+  # определение загрузочного диска
+  boot_disk {
+    initialize_params {
+      image = "${var.disk_image}"
+    }
+  }
+
+  # определение сетевого интерфейса
+  network_interface {
+    # сеть, к которой присоединить данный интерфейс
+    network = "default"
+
+    # использовать ephemeral IP для доступа из Интернет
+    access_config {}
+  }
 }
+
 resource "google_compute_firewall" "firewall_puma" {
-	name = "allow-puma-default"
-	# Название сети, в которой действует правило
-	network = "default"
-	# Какой доступ разрешить
-	allow {
-		protocol = "tcp"
-		ports = ["9292"]
-	}
-	# Каким адресам разрешаем доступ
-	source_ranges = ["0.0.0.0/0"]
-	# Правило применимо для инстансов с перечисленными тэгами
-	target_tags = ["reddit-app"]
-	}
+  name = "allow-puma-default"
+
+  # Название сети, в которой действует правило
+  network = "default"
+
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  # Каким адресам разрешаем доступ
+  source_ranges = ["0.0.0.0/0"]
+
+  # Правило применимо для инстансов с перечисленными тэгами
+  target_tags = ["reddit-app"]
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,23 @@
 variable project {
-description = "Project ID"
+  description = "Project ID"
 }
+
 variable region {
-description = "Region"
-# Значение по умолчанию
-default = "europe-west1"
+  description = "Region"
+
+  # Значение по умолчанию
+  default = "europe-west1"
 }
+
 variable public_key_path {
-# Описание переменной
-description = "Path to the public key used for ssh access"
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+variable private_key_path {
+ description = "Path to the private key used for ssh access"
 }
 variable disk_image {
-description = "Disk image"
+  # Описание переменной
+  description = "Disk image"
 }
 
