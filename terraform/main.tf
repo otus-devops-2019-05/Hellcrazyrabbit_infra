ab74f0e9a93fd7dc17ebfcf437cdb8a87dfec871
Dmitrii Sedov <vendikato@gmail.com>
2019-07-01 18:24:50 +0300

    Частичное выполнение ДЗ со *, т.к. рекомендовано сдать до 01.07

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,6 +12,16 @@ provider "google" {
   region  = "${var.region}"
 }
 
+resource "google_compute_project_metadata" "ssh-keys" {
+  metadata {
+    ssh-keys = <<EOF
+appuser:${file(var.public_key_path)}
+appuser1:${file(var.public_key_path)}
+appuser2:${file(var.public_key_path)}
+EOF
+  }
+}
+
 resource "google_compute_instance" "app" {
   name         = "reddit-app"
   machine_type = "g1-small"
